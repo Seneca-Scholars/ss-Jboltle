2d08ff9f15ef67ef6adf6368c4f32d4d6f1a3685
Jonathan Bolton <stacbolt2@gmail.com>
2024-09-03 14:59:55 -0400

    worked on the changes wajiw asked for and it works on the website now, still working on implimenting all the features required

--- SS-React-Api-Practice/react-api-app/Server/apis.js
+++ SS-React-Api-Practice/react-api-app/Server/apis.js
@@ -1,121 +1,24 @@
-const express = require('express');
-const data = require('./data.json')
+const express = require("express");
 
-
-
-
+const things = require("./things.json");
+const games = require("./games.json");
 
 const app = express();
 const port = 3000;
 
-
-
-
-
-/* 
-
-make a function that fetches  this data in a seperate file and then pass that into the Route element variable in the App.jsx file. 
-
-
-
-*/
-
-
-
-app.get('/', (req, res) => {
-
-    res.send(data);
-  
-})
-
-
-
-app.get('/id', (req, res) => {
-    
-  const id = dataID(data);
-  res.send(id);
-  
+app.get("/", (req, res) => {
+    res.send("Hello");
 });
 
-
-
-
-
-app.get('/name', (req, res) => {
-
-    const name = dataName(data);
-    res.send(name);
-
-
+app.get("/items", (req, res) => {
+    res.send(things);
+    console.log(things);
 });
 
-
-
-
-app.get('/category', (req, res) => {
-  
-  const category = dataCategory(data); 
-  res.send(category);
-  
+app.get("/games", (req, res) => {
+    res.send(games);
 });
-
-
-
-app.get('/order', (req, res) => {
-
-const order = dataOrder(data); 
-res.send(order); 
-
-});
-
-
 
 app.listen(port, () => {
-console.log(`App listening on port ${port}`);
+    console.log(`App listening on port ${port}`);
 });
-
-
-
-
-
-
-
-
-const dataName = (data) => {
-  
-  const name = data.map(item => item.name); 
-
-  return name
-};
-
-
-
-
-const dataID = (data) => {
-
-  const ids = data.map(item => item.id);
-
-  return ids;
-
-
-}
-
-const dataCategory = (data) => {
-  const category = data.map(item => item.category);
-
-  return category
-};
-
-
-
-const dataOrder = (data) => {
-
-const order = data.map(item => item.order)
-return order
-
-}
-
-
-
-
-
--- SS-React-Api-Practice/react-api-app/src/App.jsx
+++ SS-React-Api-Practice/react-api-app/src/App.jsx
@@ -1,67 +1,27 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
-import React, { useState, useEffect } from "react";
-import { ApiNameFetch } from "./apiComponent";
+import { Route, Routes } from "react-router-dom";
+import React from "react";
 
-function AppContent() {
-  const [endPoint, setEndpoint] = useState("");
-  const location = useLocation();
-
-  useEffect(() => {
-    setEndpoint(location.pathname); 
-//basically the location react hook returns an object that has a bunch of stuff lilke query pramereters, and other stuff but we use it here for the path name which looks like "/abc"
+import { ThingComponent } from "./thingComponent";
+const App = () => {
 
 
-  }, [location]);//we pass location as a dependency becauser it can change based on what we click.
-                //before we used endpoint as a dependency and set the endpoint based on a click but somehting was fricking wrong with it and idk wy
+
 
   return (
-    <div>
-      <h1 className="title">API FrontEnd</h1>
 
-      <span className="top-nav-bar">
-        <a
-          className="link"
-          href="/name"
-        >
-          Name
-        </a>
-        <a
-          className="link"
-          href="/id"
-        >
-          ID
-        </a>
-        <a
-          className="link"
-          href="/order"
-        >
-          Order
-        </a>
-        <a
-          className="link"
-          href="/category"
-        >
-          Category
-        </a>
-      </span>
+    
 
-      <Routes>
-        <Route path="/category" element={<ApiNameFetch endPoint={endPoint} />} />
-        <Route path="/order" element={<ApiNameFetch endPoint={endPoint} />} />
-        <Route path="/name" element={<ApiNameFetch endPoint={endPoint} />} />
-        <Route path="/id" element={<ApiNameFetch endPoint={endPoint} />} />
-      </Routes>
-    </div>
-  );
+    <Routes>
+  <Route path="/thing" element={<ThingComponent/>}/>
+</Routes>
+)
+
+  
+
+
 }
 
-function App() {
-  return (
-    <BrowserRouter>
-      <AppContent />
-    </BrowserRouter>
-  );
-}
+export default App
 
-export default App;
+
